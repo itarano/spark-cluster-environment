87e8853bae893fcc69f43ba5fb548ab7fa0ec7ca
Ignacio José Tárano Miranda <you@example.com>
2020-05-06 16:56:41 +0200

    refactoring counter.py

--- projects/python/word-count/counter.py
+++ projects/python/word-count/counter.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext, SparkConf
  
-conf = SparkConf().setAppName("contador").setMaster("spark://master:7077")
+conf = SparkConf().setAppName("Counter").setMaster("spark://master:7077")
 sc = SparkContext(conf=conf)
 text_file = sc.textFile("/tmp/data/lorem.txt")
 counts = text_file.flatMap(lambda linea: linea.split(" ")).countByValue()
