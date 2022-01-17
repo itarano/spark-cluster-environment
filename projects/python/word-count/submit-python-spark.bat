3fcfed4e89d8a87504de9cdc3891b23aae5edd8b
Nacho Tárano <ignaciojose.tarano@gmail.com>
2022-01-17 19:09:30 +0100

    chore: Removed python .bat submitter

--- projects/python/word-count/submit-python-spark.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set file=%1
-if [%file%]==[] (echo Need a script to run) else (copy %1 "..\..\..\docker\volumes\spark-master\data" && docker exec -it spark-master spark-submit /tmp/data/%file:.\=%)
